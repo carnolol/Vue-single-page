58f5bd808b2975d6d8e7f0904ec5c0493a9031c6
Mike Chadwick <mchadwick@blenderbottle.com>
2020-12-28 10:21:20 -0700

    my msg

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hero">
+        <h3>Hero.vue</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Hero"
+}
+</script>
+
+<style lang="scss">
+@import '../styles/Variables.scss';
+@import '../styles/Hero.scss';
+
+</style>
